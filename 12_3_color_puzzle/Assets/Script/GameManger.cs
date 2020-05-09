e37f2e09f87fa076ba1181de72921311c0be81e6
kacias <kacias@daum.net>
2020-05-09 17:16:55 +0900

    puzzle update

---
+++ 12_3_color_puzzle/Assets/Script/GameManger.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+
+public class GameManger : MonoBehaviour
+{
+    public GameObject block0;
+    public GameObject block1;
+    public GameObject block2;
+
+    public int[,] array = new int[3, 3];
+
+
+    public int score = 0;
+    public Text countText;
+
+    private int time = 100;
+    public Text TimeText;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        array[0, 0] = 0;
+        array[0, 1] = 0;
+        array[0, 2] = 0;
+
+        array[1, 0] = 1;
+        array[1, 1] = 1;
+        array[1, 2] = 1;
+
+        array[2, 0] = 2;
+        array[2, 1] = 2;
+        array[2, 2] = 2;
+                                       
+        for (int x=0; x<3; x++)
+        {
+            for (int y=0; y<3; y++)
+            {
+                Debug.Log("[x,y]" + array[x, y]);
+
+                float x_pos = x * 1.0f;
+                float y_pos = y * 1.0f;
+
+                if (array[x,y] == 0)
+                {     
+                    Instantiate(block0, new Vector3(x_pos,y_pos,0), Quaternion.identity);
+
+                }
+                else if (array[x,y] == 1)
+                {     
+                    Instantiate(block1, new Vector3(x_pos,y_pos,0), Quaternion.identity);
+    
+                }
+                else if (array[x,y] == 2)
+                {   
+                    Instantiate(block2, new Vector3(x_pos,y_pos,0), Quaternion.identity);
+
+                }
+            }
+        }
+    }
+
+    //====================================================================
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D clickColl = null;
+        Vector3 worldPos;
+        Vector2 clickPos;
+
+
+        if  (time >= 1)
+        {
+            time -= 1;
+            TimeText.text = "Time: " + time.ToString();
+
+        }
+        else
+        {
+
+            SceneManager.LoadScene("ending");
+
+
+
+        }
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            clickPos = new Vector2(worldPos.x, worldPos.y);
+            clickColl = Physics2D.OverlapPoint(clickPos);
+
+            //Debug.Log(clickColl.gameObject);
+
+            try{            
+
+                if (clickColl.gameObject.tag == "Block1")
+                {
+                    score += 1;
+                    countText.text = "Count: " + score.ToString();
+                    Debug.Log("Block1"); 
+                }
+                else if (clickColl.gameObject.tag == "Block2")
+                {
+                    score += 2;
+                    countText.text = "Count: " + score.ToString();
+
+
+                    Debug.Log("Block2"); 
+                }
+                else if (clickColl.gameObject.tag == "Block3")
+                {
+
+                    score += 3;
+                    countText.text = "Count: " + score.ToString();
+
+                    Debug.Log("Block3");
+                }
+                else {
+
+                    Debug.Log("no object!"); 
+
+                }
+
+            }catch(NullReferenceException ex) {
+
+                Debug.Log(ex);
+
+                //Debug.Log("myLight was not set in the inspector");
+            }
+            
+        }
+
+    }
+}
---
+++ Unity_Adv_Class/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+
+
+
+public class UI_Manager : MonoBehaviour
+{
+    private const string android_game_id = "3585448";
+    private const string ios_game_id = "3585448";
+
+    private const string rewarded_video_id = "rewardedVideo";
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+#if UNITY_ANDROID
+        Advertisement.Initialize(android_game_id);
+#elif UNITY_IOS
+        Advertisement.Initialize(ios_game_id);
+#elif UNITY_EDITOR
+        Advertisement.Initialize(ios_game_id);
+
+#endif
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady(rewarded_video_id))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+
+            Advertisement.Show(rewarded_video_id, options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                {
+                    Debug.Log("The ad was successfully shown.");
+
+                    // to do ...
+                    // 광고 시청이 완료되었을 때 처리
+
+                    break;
+                }
+            case ShowResult.Skipped:
+                {
+                    Debug.Log("The ad was skipped before reaching the end.");
+
+                    // to do ...
+                    // 광고가 스킵되었을 때 처리
+
+                    break;
+                }
+            case ShowResult.Failed:
+                {
+                    Debug.LogError("The ad failed to be shown.");
+
+                    // to do ...
+                    // 광고 시청에 실패했을 때 처리
+
+                    break;
+                }
+        }
+    }
+
+}
