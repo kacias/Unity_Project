fcfe0f8079b9196fbbc87014012ebdfadca1ea23
kacias <kacias@daum.net>
2020-06-08 11:19:04 +0900

    factory_pattern_added

---
+++ 15_3_color_puzzle_Refactoring2/Assets/Script/Factory_Pattern.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory_Pattern : MonoBehaviour
+{
+    public static Factory_Pattern instance = null;
+
+    public GameObject red_monster1_prefab;
+    public GameObject green_monster1_prefab;
+
+
+    void Awake()
+    {
+        //싱글톤
+        instance = this;
+    }
+
+    public void CreateMonster(string type)
+    {
+
+        if(type.Equals("red"))
+        {
+            
+            Instantiate(red_monster1_prefab, new Vector3(10,0,0), Quaternion.identity);
+
+        }
+        else if(type.Equals("green"))
+        {            
+
+            Instantiate(green_monster1_prefab, new Vector3(-10,0,0), Quaternion.identity);
+
+        }
+
+    }
+
+}
--- 15_3_color_puzzle_Refactoring2/Assets/Script/Monster.cs
+++ 15_3_color_puzzle_Refactoring2/Assets/Script/Monster.cs
@@ -7,12 +7,27 @@ public class Monster : MonoBehaviour
     Abstract_Class monster1 = new Abstract_Class();
     Monster_Sub monster2 = new Monster_Sub(); 
 
+    //public GameObject red_monster1_prefab;
+    //public GameObject green_monster1_prefab;
+         
     // Start is called before the first frame update
     void Start()
     {
         monster1.name = "Dragon";
         monster1.hp = 100;
     
+
+        //Red_Monster greenGoblin = new Red_Monster();
+        
+        //일반 코딩 
+        //Instantiate(red_monster1_prefab, new Vector3(10,0,0), Quaternion.identity);
+        //Instantiate(green_monster1_prefab, new Vector3(-10,0,0), Quaternion.identity);
+
+        //팩토리 패턴으로 생성 
+        Factory_Pattern.instance.CreateMonster("red");
+        Factory_Pattern.instance.CreateMonster("green");
+
+
 
     }
 
@@ -21,5 +36,6 @@ public class Monster : MonoBehaviour
     {
          //monster1.Debug_Log();
          monster2.Debug_Log_Override();        
-    }
+    }       
+
 }
