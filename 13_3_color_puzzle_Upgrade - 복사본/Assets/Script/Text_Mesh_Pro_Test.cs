bc69da09fa485dc9f8e9ec9f9d7795a0fcd3e61e
kacias <53470217+kacias@users.noreply.github.com>
2021-05-22 15:02:39 +0900

    수업 준비용 퍼즐 코드

---
+++ 13_3_color_puzzle_Upgrade - 복사본/Assets/Script/Text_Mesh_Pro_Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // Add the TextMesh Pro namespace to access the various functions.
+
+
+public class Text_Mesh_Pro_Test : MonoBehaviour
+{
+
+    TextMeshProUGUI resourceText;
+    private int resource;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resourceText = GetComponent<TextMeshProUGUI>();
+
+        //resource = 1000;
+        //Debug.Log(GameManger.score); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        resource = GameManger.score;
+        resourceText.text = "Count: " + resource.ToString();
+    }
+
+        
+}
