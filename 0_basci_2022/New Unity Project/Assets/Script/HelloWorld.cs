ccc9be7e66e0be0b0aaead5294de4572d39111d1
kacias <53470217+kacias@users.noreply.github.com>
2022-03-12 21:49:47 +0900

    2022년 버전 추가

---
+++ 0_basci_2022/New Unity Project/Assets/Script/HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world"); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.Log("I am on Update loop");
+
+    }
+}
